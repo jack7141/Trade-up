a0a9264277ac6fe5a1895a922356c6662ea4e6ad
황광회 <ghl92479@gmail.com>
2025-08-21 16:33:04 +0900

    ADD: 스플레시 이미지

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:trade_up/l10n/app_localizations.dart';
 
@@ -8,14 +9,37 @@ import 'core/router/app_router.dart';
 import 'core/theme/app_theme.dart';
 
 void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+
+  // Native splash를 유지하고 Flutter가 준비되면 제거
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
   runApp(ProviderScope(child: const TradeUpApp()));
 }
 
-class TradeUpApp extends ConsumerWidget {
+class TradeUpApp extends ConsumerStatefulWidget {
   const TradeUpApp({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<TradeUpApp> createState() => _TradeUpAppState();
+}
+
+class _TradeUpAppState extends ConsumerState<TradeUpApp> {
+  @override
+  void initState() {
+    super.initState();
+    // 실제 기기에서 더 안정적인 splash 제거를 위해 지연 시간 추가
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      Future.delayed(const Duration(milliseconds: 1000), () {
+        if (mounted) {
+          FlutterNativeSplash.remove();
+        }
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final currentLanguage = ref.watch(languageProvider);
 
     return MaterialApp.router(
